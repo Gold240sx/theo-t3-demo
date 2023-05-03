feefee46020d94165fd7edff37a268c41b6793f5
Gold240sx <240designworks@gmail.com>
2023-05-03 04:28:06 -0500

    2. test

--- src/env.mjs
+++ src/env.mjs
@@ -27,6 +27,8 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
 });
